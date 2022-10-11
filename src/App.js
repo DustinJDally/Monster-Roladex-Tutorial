f7e4e46e2f614e61a8faa20abd63d093902546af
Dustin Dally <dj.dally@protonmail.com>
2022-10-11 17:23:00 -0600

    Update App.js
    Minor changes per tutorial

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import './App.css';
 import logo from './logo.svg';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "Dustin"
+    }
+
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Sup Bitch
-          </p>
-          <button>
+          <p>Sup {this.state.name}</p>
+          <button onClick={()=>{ 
+            this.state.name = 'James';
+            console.log(this.state);
+          }}>
             Change Name
           </button>
         </header>
