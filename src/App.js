85289e377d3928700208b11a8a1b117ac47a8202
Dustin Dally <dj.dally@protonmail.com>
2022-10-18 17:53:24 -0600

    Started on the Roladex
    Started to code it via the tutorial

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { findAllByAltText } from '@testing-library/react';
 import { Component } from 'react';
 import './App.css';
 import logo from './logo.svg';
@@ -7,27 +8,33 @@ class App extends Component {
     super();
 
     this.state = {
-      name: "Dustin"
-    }
-
+      monsters: [],
+    };
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => console.log(users))
+  } 
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Sup {this.state.name}</p>
-          <button onClick={()=>{ 
-            this.state.name = 'James';
-            console.log(this.state);
-          }}>
-            Change Name
-          </button>
-        </header>
+        {this.state.monsters.map( (monster)=> {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+          })}
       </div>
     );
   }
 }
 
 export default App;
+
+const fast = () => {
+  
+}
