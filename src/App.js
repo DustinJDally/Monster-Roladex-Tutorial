01b9f77788c480bf102af703e7171bd21e1e2cc4
Dustin Dally <dj.dally@protonmail.com>
2022-11-05 17:49:13 -0600

    Continuing Functional Comps

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
+import { useEffect, useState } from 'react';
+// import { Component } from 'react';  USED ON CLASS
 import { findAllByAltText } from '@testing-library/react';
-import { Component } from 'react';
 import './App.css';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 
+// FUNCTIONAL COMPONENTS
+
 const App = () => {
+  const [searchField, setSearchField] = useState(''); //[value, setValue]
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters]= useState(monsters);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => setMonsters(users));    
+    }, []);
+
+    useEffect(() => {
+        const newFilteredMonsters = monsters.filter((monster) => {
+          return monster.name.toLocaleLowerCase().includes(searchField);
+      });
+
+      setFilteredMonsters(newFilteredMonsters);
+
+    }, [monsters, searchField]);
+
+  const onSearchChange = (event) => {
+        const searchFieldString = event.target.value.toLocaleLowerCase(); 
+        setSearchField(searchFieldString);
+      };    
+
+
   return (
     <div className="App">
       <h1 className="app-title">Monsters Rolodex</h1>
-      {/* <SearchBox 
+
+      <SearchBox 
         onChangeHandler={onSearchChange} 
         placeholder='search monsters' 
         className='monsters-search-box' 
       />
-      <CardList monsters={filteredMonsters} /> */}
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 };
+
+
+// CLASS COMPONENTS
+
 // class App extends Component {
 //   constructor() {
 //     super();
