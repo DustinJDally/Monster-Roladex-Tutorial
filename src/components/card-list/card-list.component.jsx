e1b14f99ee4a663a859daae8b28d449fa66020d7
Dustin Dally <dj.dally@protonmail.com>
2022-11-06 12:02:02 -0700

    Finished Function converstion

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,20 +1,33 @@
-import { Component } from "react";
+// import { Component } from "react";Used in Class
 import Card from '../card/card.component';
 import './card-list.styles.css';
 
-class CardList extends Component {
-    render (){
-        const { monsters } = this.props;
-        console.log(this.props.monsters);
-        console.log('render from Cardlist');
-        return (
-            <div className='card-list'> 
-                {monsters.map((monster) => {
-                    return <Card monster={monster}/>;
-                })}
-            </div>
-        );
-    }
-}
+const CardList = ({ monsters }) => (
+    <div className='card-list'> 
+        {monsters.map((monster) => {
+            return <Card monster={monster}/>;
+        })}
+    </div>
+    );
+
+
+
+
+
+//  CLASS COMPONENTS
+// class CardList extends Component {
+//     render (){
+//         const { monsters } = this.props;
+//         console.log(this.props.monsters);
+//         console.log('render from Cardlist');
+//         return (
+//             <div className='card-list'> 
+//                 {monsters.map((monster) => {
+//                     return <Card monster={monster}/>;
+//                 })}
+//             </div>
+//         );
+//     }
+// }
 
 export default CardList;
